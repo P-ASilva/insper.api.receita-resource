a4a2ac816994e053af82c3ce4b1b56ab37d14508
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2024-04-10 08:49:17 -0300

    swagger test

--- src/main/java/insper/api/receitas/ReceitasResource.java
+++ src/main/java/insper/api/receitas/ReceitasResource.java
@@ -1,51 +1,18 @@
 package insper.api.receitas;
 
-import java.util.Map;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import insper.api.ingrediente.IngredienteIn;
-import insper.api.ingrediente.IngredienteOut;
+import ch.qos.logback.core.model.ComponentModel;
 
 @RestController
 public class ReceitasResource implements ReceitasController {
 
     @Autowired
     private ReceitasService receitasService;
-
-    @GetMapping("/receitas/info")
-    public ResponseEntity<Map<String, String>> info() {
-        return new ResponseEntity<Map<String, String>>(
-            Map.ofEntries(
-                Map.entry("microservice.name", ReceitasApplication.class.getSimpleName()),
-                Map.entry("os.arch", System.getProperty("os.arch")),
-                Map.entry("os.name", System.getProperty("os.name")),
-                Map.entry("os.version", System.getProperty("os.version")),
-                Map.entry("file.separator", System.getProperty("file.separator")),
-                Map.entry("java.class.path", System.getProperty("java.class.path")),
-                Map.entry("java.home", System.getProperty("java.home")),
-                Map.entry("java.vendor", System.getProperty("java.vendor")),
-                Map.entry("java.vendor.url", System.getProperty("java.vendor.url")),
-                Map.entry("java.version", System.getProperty("java.version")),
-                Map.entry("line.separator", System.getProperty("line.separator")),
-                Map.entry("path.separator", System.getProperty("path.separator")),
-                Map.entry("user.dir", System.getProperty("user.dir")),
-                Map.entry("user.home", System.getProperty("user.home")),
-                Map.entry("user.name", System.getProperty("user.name")),
-                Map.entry("jar", new java.io.File(
-                    ReceitasApplication.class.getProtectionDomain()
-                        .getCodeSource()
-                        .getLocation()
-                        .getPath()
-                    ).toString())
-            ), HttpStatus.OK
-        );
-    }
 
     @Override
     public ResponseEntity<ReceitaOut> create(ReceitaIn in) {
@@ -61,12 +28,6 @@ public class ReceitasResource implements ReceitasController {
                 .buildAndExpand(receita.id())
                 .toUri())
             .body(ReceitasParser.to(receita));
-    }
-
-    @Override
-    public ResponseEntity<ReceitaOut> update(String id, ReceitaIn in) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
     @Override
@@ -86,6 +47,34 @@ public class ReceitasResource implements ReceitasController {
     public int getQuantidadeIngrediente(String id, String idIngrediente) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getQuantidadeIngrediente'");
+    }
+
+    @Override
+    public List<ComponentModel> getIngredientes(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getIngredientes'");
+    }
+
+    @Override
+    public List<ComponentModel> getReceitas(String idIngrediente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getReceitas'");
+    }
+
+    @Override
+    public ResponseEntity<ReceitaOut> update(String id, ReceitaIn in) {
+        // parser
+        Receita receita = ReceitasParser.to(in);
+        // insert using service
+        receita = receitasService.update(id, receita);
+        // return
+        return ResponseEntity.created(
+            ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(receita.id())
+                .toUri())
+            .body(ReceitasParser.to(receita));
     }
 
 }
